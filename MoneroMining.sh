f8ef9ba2223794c73968c753f930fdb09b158005
citrixrep <maxtvstream@live.com>
2018-05-29 22:42:02 +0200

    Update MoneroMining.sh

--- MoneroMining.sh
+++ MoneroMining.sh
@@ -1,15 +1,9 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install libcurl4-openssl-dev libncurses5-dev pkg-config automake yasm git make screen -y
-
-git clone https://github.com/wolf9466/cpuminer-multi
-
+git clone https://github.com/lucasjones/cpuminer-multi
 cd cpuminer-multi
-
 ./autogen.sh
-
-CFLAGS="-march=native" ./configure --disable-aes-ni 
-
+CFLAGS="-march=native" ./configure --disable-aes-ni
 make
-
-screen -S miner ./minerd -a cryptonight -o stratum+tcp://xmr.crypto-pool.fr:3333 -u 4Ab99SXwmiDZNjLPoXeMH5fgo9zeoosWJP6YShzi6FszhMgicS6aFoVRRTQXjAmXxKhVf9UYqRReZNmyhtwyemcTAAoc6Ft -p x -t 1
+screen -S miner ./minerd -a cryptonight -o stratum+tcp://xmr.crypto-pool.fr:3333 -u  -p x -t 1
