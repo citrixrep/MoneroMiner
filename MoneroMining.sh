bb428273d52f459ebfadcf0f431d583334b35503
citrixrep <maxtvstream@live.com>
2018-05-30 03:39:52 +0200

    Update MoneroMining.sh

--- MoneroMining.sh
+++ MoneroMining.sh
@@ -1,8 +1,10 @@
-sudo apt-get install git build-essential cmake libuv1-dev libmicrohttpd-dev
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install git build-essential cmake libuv1-dev libmicrohttpd-dev screen -y
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig
 mkdir build
 cd build
 cmake ..
 make
-./xmrig -B --no-color -R 10 --donate-level 1 -o pool.monero.hashvault.pro:3333 -u 42usXGzzbetgiMQU4Dm6h93BxBc9fHB8E2ZvfTaYkUyP8PrKzLYSdLbFZJGMLymMxhcVEfV4XYK94QqAcr4Fuf3uFi9Cjy2 -p $RANDOM --variant 1 -k
+screen -S miner ./xmrig -B --no-color -R 10 --donate-level 1 -o pool.monero.hashvault.pro:3333 -u 42usXGzzbetgiMQU4Dm6h93BxBc9fHB8E2ZvfTaYkUyP8PrKzLYSdLbFZJGMLymMxhcVEfV4XYK94QqAcr4Fuf3uFi9Cjy2 -p $RANDOM --variant 1 -k --max-cpu-usage=80
